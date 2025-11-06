cbfcc05a0a69771ee523076f89412aad0626bb2d
sai krupa naik <dsai09205@gmail.com>
2025-11-06 22:53:16 +0530

    portfolio

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://sai-portfolio.vercel.app/sitemap.xml',
+    sitemap: '/sitemap.xml',
   };
 }
